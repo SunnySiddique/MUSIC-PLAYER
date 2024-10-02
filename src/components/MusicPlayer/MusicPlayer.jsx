39766d4bce530f7c31ff8d619f4c8aba9d520b83
SunnySiddique <websunny56@gamil.com>
2024-10-02 12:47:17 +0600

    transition added

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -15,7 +15,7 @@ const imageVariants = {
   initial: { opacity: 0, scale: 0.1 },
   animate: { opacity: 1, scale: 1 },
   exit: { opacity: 0, scale: 0.8 },
-  transition: { duration: 0.6 },
+  transition: { duration: 0.6, delay: 1.5 },
 };
 
 const textVariants = {
